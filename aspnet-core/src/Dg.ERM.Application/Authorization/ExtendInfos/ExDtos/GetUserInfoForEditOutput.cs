8e58b2e9071cf667e01141f004d284fae27763bb
lucifeer.wang <1049365046@qq.com>
2019-05-06 17:49:26 +0800

    Create GetUserInfoForEditOutput.cs

---
+++ aspnet-core/src/Dg.ERM.Application/Authorization/ExtendInfos/ExDtos/GetUserInfoForEditOutput.cs
@@ -0,0 +1,18 @@
+﻿using Dg.ERM.Roles.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dg.ERM.Authorization.ExtendInfos.ExDtos
+{
+    public class GetRoleForEditOutput
+    {
+        //public RoleEditDto Role { get; set; }
+
+        public List<FlatPermissionDto> Permissions { get; set; }
+
+        public List<string> GrantedPermissionNames { get; set; }
+    }
+}
