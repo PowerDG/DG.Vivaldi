7b566ffe106cc8a899a377483f3b4cb7ef7fe445
lucifeer.wang <1049365046@qq.com>
2019-05-07 13:15:33 +0800

    Update UserDto.cs

--- aspnet-core/src/Dg.ERM.Application/Users/Dto/UserDto.cs
+++ aspnet-core/src/Dg.ERM.Application/Users/Dto/UserDto.cs
@@ -1,12 +1,13 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 using Abp.Application.Services.Dto;
 using Abp.Authorization.Users;
 using Abp.AutoMapper;
 using Dg.ERM.Authorization.Users;
 
 namespace Dg.ERM.Users.Dto
-{
+{ 
     [AutoMapFrom(typeof(User))]
     public class UserDto : EntityDto<long>
     {
@@ -36,5 +37,10 @@ namespace Dg.ERM.Users.Dto
         public DateTime CreationTime { get; set; }
 
         public string[] RoleNames { get; set; }
+
+        //public static implicit operator UserDto(Task<User> v)
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
