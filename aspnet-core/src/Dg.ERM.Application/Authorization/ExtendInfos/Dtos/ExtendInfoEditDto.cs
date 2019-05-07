6dbd02c1df680753d3c2b67034743fea44c4acd1
lucifeer.wang <1049365046@qq.com>
2019-05-07 13:15:28 +0800

    Update ExtendInfoEditDto.cs

--- aspnet-core/src/Dg.ERM.Application/Authorization/ExtendInfos/Dtos/ExtendInfoEditDto.cs
+++ aspnet-core/src/Dg.ERM.Application/Authorization/ExtendInfos/Dtos/ExtendInfoEditDto.cs
@@ -7,6 +7,87 @@ using Dg.ERM.Authorization.ExtendInfos;
 
 namespace  Dg.ERM.Authorization.ExtendInfos.Dtos
 {
+
+    [AutoMap(typeof(ExtendInfo))]
+    public class ExtendInfoDto
+    {
+
+        /// <summary>
+        /// Id
+        /// </summary>
+        //public long? Id { get; set; }
+
+
+
+        /// <summary>
+        /// TenantId
+        /// </summary>
+        public int TenantId { get; set; }
+         
+        ///// <summary>
+        ///// ParentId
+        ///// </summary>
+        //public int ParentId { get; set; } 
+        /// <summary>
+        /// Super_Type
+        /// </summary>
+        public string Super_Type { get; set; }
+        ///// <summary>
+        ///// EntityTypeFullName
+        ///// </summary>
+        //public string EntityTypeFullName { get; set; }
+
+
+        public long EnityID { get; set; }
+
+        ///// <summary>
+        ///// Name
+        ///// </summary>
+        ////[Required(ErrorMessage = "Name不能为空")]
+        //public string Name { get; set; }
+
+
+        ///// <summary>
+        ///// Value
+        ///// </summary>
+        //public string Value { get; set; }
+
+        /// <summary>
+        /// DataTypeName
+        /// </summary>
+        //public string DataTypeName { get; set; }
+
+
+
+        public string PropertyName { get; set; }
+        public string PropertyValue { get; set; }
+
+        public string PropertyTypeName { get; set; }
+
+        ///// <summary>
+        ///// Description
+        ///// </summary>
+        //public string Description { get; set; }
+        ///// <summary>
+        ///// DisplayName
+        ///// </summary>
+        //public string DisplayName { get; set; }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
     [AutoMap(typeof(ExtendInfo))]
     public class ExtendInfoEditDto
     {
