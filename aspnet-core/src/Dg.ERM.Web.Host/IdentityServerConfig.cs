0525f5f9d660a4c44bad80530ef7abf7e7697996
lucifeer.wang <1049365046@qq.com>
2019-05-08 10:27:08 +0800

    Create IdentityServerConfig.cs

---
+++ aspnet-core/src/Dg.ERM.Web.Host/IdentityServerConfig.cs
@@ -0,0 +1,48 @@
+﻿//using Castle.Core.Resource;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace Dg.ERM.Web.Host
+//{
+//    public static class IdentityServerConfig
+//    {
+//        public static IEnumerable<IResource> GetApiResources()
+//        {
+//            return new List<ApiResource>
+//        {
+//            new ApiResource("default-api", "Default (all) API")
+//        };
+//        }
+
+//        public static IEnumerable<IdentityResource> GetIdentityResources()
+//        {
+//            return new List<IdentityResource>
+//        {
+//            new IdentityResources.OpenId(),
+//            new IdentityResources.Profile(),
+//            new IdentityResources.Email(),
+//            new IdentityResources.Phone()
+//        };
+//        }
+
+//        public static IEnumerable<Client> GetClients()
+//        {
+//            return new List<Client>
+//        {
+//            new Client
+//            {
+//                ClientId = "client",
+//                AllowedGrantTypes = GrantTypes.ClientCredentials.Union(GrantTypes.ResourceOwnerPassword).ToList(),
+//                AllowedScopes = {"default-api"},
+//                ClientSecrets =
+//                {
+//                    new Secret("secret".Sha256())
+//                }
+//            }
+//        };
+//        }
+//    }
+
+//}
